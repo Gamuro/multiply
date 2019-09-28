9e9f4096b02be1a7001efd97c25a3506c5d54720
EvgeniyGorbunov <yhgamuro@gmail.com>
2019-09-28 13:22:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let firstArr = first.split('').reverse();
+    let secondArr = second.split('').reverse();
+    let fullArr = [];
+    for (let i = 0; i < firstArr.length; i++) {
+        for (let j = 0; j < secondArr.length; j++) {
+            if (fullArr[i + j]) {
+                fullArr[i + j] += firstArr[i] * secondArr[j];
+            } else {
+                fullArr[i + j] = firstArr[i] * secondArr[j];
+            }
+
+        }
+    }
+
+
+    for (let i = 0; i < fullArr.length; i++) {
+        let num = fullArr[i] % 10;
+        let subnum = Math.floor(fullArr[i] / 10);
+        fullArr[i] = num;
+        if (fullArr[i + 1]) {
+            fullArr[i + 1] += subnum;
+        } else if (subnum !== 0) {
+            fullArr[i + 1] = subnum;
+        }
+    }
+
+    return fullArr.reverse().join('');
 }
